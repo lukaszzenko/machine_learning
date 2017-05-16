1232aa9479703ab695c7d6cc1364dfed9fb3fbd9
lukaszzenko <lukasz.jocz@gmail.com>
2017-05-16 15:39:04 +0200

    Add featureNormalize.m and normalEqn.m

---
+++ linear_regression/machine-learning-ex1/featureNormalize.m
@@ -0,0 +1,19 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% Compute the mean for each feature and subtract it from the dataset
+mu = mean(X);
+mu(1) = 0;  % Leave ones in the first column untouched 
+X_norm = X - mu;
+
+% Compute the standard deviation of each feature and divide each feature by it's standard deviation
+sigma = std(X_norm);
+sigma(sigma == 0) = 1; % Replace 0s to 1s to avoid diving by 0
+
+X_norm = X_norm ./ sigma;
+
+end
--- linear_regression/machine-learning-ex1/runGradientDescent.m
+++ linear_regression/machine-learning-ex1/runGradientDescent.m
@@ -1,7 +1,8 @@
 function runGradientDescent(fileName)
     data = load(fileName);
 
-    X = [ones(size(data, 1), 1), data(:, 1:end - 1)]; % Add a column of ones to x
+    X_ = [ones(size(data, 1), 1), data(:, 1:end - 1)]; % Add a column of ones to x
+    X =  featureNormalize(X_); % Normalize features
     y = data(:, end:end);
 
     theta = zeros(size(X, 2), 1); % initialize fitting parameters
@@ -10,15 +11,20 @@ function runGradientDescent(fileName)
     iterations = 1500;
     alpha = 0.01;
 
-    theta = gradientDescent(X, y, theta, alpha, iterations);
+    [theta, J_history] = gradientDescent(X, y, theta, alpha, iterations);
 
     if size(X, 2) == 2
       % Plot Data
       plotData(X(:, 2), y);
       range = xlim()(1) : 0.1 : xlim()(2);
-      disp(xlim());
       hold on;
       plot(range, [ones(length(range), 1), range'] * theta);
+      theta_2 = normalEqn(X, y);
+      disp(theta);
+      disp(theta_2);
+      plot(range, [ones(length(range), 1), range'] * theta_2);
     end
 
+    figure;
+    plot(J_history);
 end
